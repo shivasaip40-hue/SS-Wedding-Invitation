57392cfca1d8c9bb9b3f3661f73428292d54646b
shivasaip40-hue <shivasaip40@gmail.com>
2026-06-24 06:15:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Welcome to the Wedding of Shiva Sai Prasad ♥ Sadha Sri");
+document.addEventListener("DOMContentLoaded", function(){
+
+    const seal = document.querySelector(".seal");
+    const invitation = document.getElementById("invitation");
+
+    seal.addEventListener("click", function(){
+
+        invitation.style.display = "block";
+
+        seal.style.transform = "scale(1.1)";
+        seal.style.transition = "0.5s";
+
+    });
+
+});
